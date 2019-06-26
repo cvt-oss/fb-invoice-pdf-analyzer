a584b3f83e8d8ad41782c8459fe9e47b0bc9d64b
Anton Giertli <agiertli@redhat.com>
2019-06-26 15:15:46 +0200

    external network

---
+++ redeploy.sh
@@ -0,0 +1,6 @@
+docker-compose down --volumes
+docker network rm cvt-oss 
+docker network create cvt-oss 
+mvn clean package -Pnative -Dnative-image.container-runtime=docker
+docker-compose up --build -d
+docker logs -f invoice-pdf-analyzer-container
